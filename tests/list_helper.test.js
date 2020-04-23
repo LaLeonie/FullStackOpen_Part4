79aab7a3c693a40b29bf2df29e27d51fcb83186c
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2020-04-23 15:16:04 +0100

    Part4 blog step 5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,4 +1,10 @@
-const { dummy, totalLikes, favoriteBlog } = require("../utils/list_helper");
+const {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+} = require("../utils/list_helper");
 
 const listWithNoBlog = [];
 const listWithOneBlog = [
@@ -24,6 +30,39 @@ const listWithMultipleBlogs = [
     title: "Animal Farm",
     author: "Orwell",
     likes: 4,
+  },
+];
+
+const listWithMultipleAuthorPosts = [
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    likes: 5,
+  },
+  {
+    title: "Harry Potter",
+    author: "Jk Rowling",
+    likes: 1,
+  },
+  {
+    title: "Champer of Secrets",
+    author: "Jk Rowling",
+    likes: 1,
+  },
+  {
+    title: "Animal Farm",
+    author: "Orwell",
+    likes: 4,
+  },
+  {
+    title: "1984",
+    author: "Orwell",
+    likes: 1,
+  },
+  {
+    title: "Other",
+    author: "Orwell",
+    likes: 5,
   },
 ];
 
@@ -66,5 +105,44 @@ describe("favorite blog", () => {
 
   test("returns string when list has no entry", () => {
     expect(favoriteBlog(listWithNoBlog)).toBe("No post found");
+  });
+});
+
+describe("most blogs", () => {
+  test("returns string when no blog posts are in array", () => {
+    expect(mostBlogs(listWithNoBlog)).toBe("No post found");
+  });
+
+  test("returns only author object when list contains one blog post", () => {
+    expect(mostBlogs(listWithOneBlog)).toEqual({
+      author: "Edsger W. Dijkstra",
+      blogs: 1,
+    });
+  });
+
+  test("returns object of author with most posts", () => {
+    expect(mostBlogs(listWithMultipleAuthorPosts)).toEqual({
+      author: "Orwell",
+      blogs: 3,
+    });
+  });
+});
+
+describe("most likes", () => {
+  test("returns string when list is empty", () => {
+    expect(mostLikes(listWithNoBlog)).toBe("No post found");
+  });
+
+  test("returns object with likes and author if only one element in list", () => {
+    expect(mostLikes(listWithOneBlog)).toEqual({
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    });
+  });
+  test("returns object from other with most likes when multiple blog entries", () => {
+    expect(mostLikes(listWithMultipleAuthorPosts)).toEqual({
+      author: "Orwell",
+      likes: 10,
+    });
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,8 +14,48 @@ const favoriteBlog = (blogs) => {
   return sortedBlog[0];
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return "No post found";
+  const authors = blogs.map((post) => post.author);
+  const collection = [];
+  for (let i = 0; i < authors.length; i++) {
+    const author = authors[i];
+    const object = collection.filter((el) => el.author === author);
+    if (object.length > 0) {
+      const index = collection.indexOf(object[0]);
+      collection[index].blogs++;
+    } else {
+      collection.push({ author, blogs: 1 });
+    }
+  }
+  const sortedCollection = collection.sort((a, b) => b.blogs - a.blogs);
+  return sortedCollection[0];
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return "No post found";
+  const collection = [];
+  for (let i = 0; i < blogs.length; i++) {
+    const author = blogs[i].author;
+    const object = collection.filter((el) => el.author === author);
+    if (object.length > 0) {
+      const index = collection.indexOf(object[0]);
+      collection[index].likes += blogs[i].likes;
+    } else {
+      collection.push(blogs[i]);
+    }
+  }
+  const sortedCollection = collection.sort((a, b) => b.likes - a.likes);
+  return {
+    author: sortedCollection[0].author,
+    likes: sortedCollection[0].likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
